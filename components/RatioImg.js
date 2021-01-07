ab5a622523a801791551b7a7a28897fdd5d27f70
lmachens <leon@machens.koeln>
2021-01-07 14:50:15 +0100

    ⚡️ Set img width and height if CSS is not loaded

--- components/RatioImg.js
+++ components/RatioImg.js
@@ -6,7 +6,7 @@ function RatioImg({ smallSrc, mediumSrc, largeSrc, alt }) {
       <picture>
         {largeSrc && <source srcSet={largeSrc} media="(min-width: 992px)" />}
         {mediumSrc && <source srcSet={mediumSrc} media="(min-width: 768px)" />}
-        <img src={smallSrc} alt={alt} />
+        <img src={smallSrc} alt={alt} width="100%" height="100%" />
       </picture>
     </div>
   );
