653f7a01e9664b0ad2735d399269cb273eef1bc0
lmachens <leon@machens.koeln>
2021-01-09 00:49:49 +0100

    ⚡️ Purge unused CSS

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    [
+      "@fullhuman/postcss-purgecss",
+      {
+        content: [
+          "./pages/**/*.{js,jsx,ts,tsx}",
+          "./components/**/*.{js,jsx,ts,tsx}",
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ],
+  ],
+};
